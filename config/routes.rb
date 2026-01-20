84ba70dd97dc915024b6d2123a20bd081e1bfb5b
Mahmud Md Fuad <102977353+fuadaman@users.noreply.github.com>
2026-01-20 21:51:27 +0900

    added thee root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+root to: 'lists#index'
   # - As a user, I can see all my movie lists (index)
   # - As a user, I can see the details of a movie list (show)
   resources :lists, only: [:index, :show] do
